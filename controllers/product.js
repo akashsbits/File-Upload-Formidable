a5ecc401143c32e5124cf1f98cf8d7753f5e1e46
Akash Srivastava <sri.akash1992@gmail.com>
2019-05-31 21:12:53 +0530

    Add HTML form for Apis integration

--- controllers/product.js
+++ controllers/product.js
@@ -1,14 +1,17 @@
+const status = require("statuses");
 const Product = require("../models/Product");
 // const { CustomAPIError, NotFoundError, BadRequestError } = require("../errors");
 
 const createProduct = async (req, res) => {
   const product = await Product.create(req.body);
 
-  res.status(200).json({ product });
+  res.status(status("OK")).json({ product });
 };
 
-const getAllProducts = (req, res) => {
-  res.status(200).send("Get All Product");
+const getAllProducts = async (req, res) => {
+  const products = await Product.find({});
+
+  res.status(status("OK")).json({ products });
 };
 
 module.exports = { createProduct, getAllProducts };
--- controllers/uploads.js
+++ controllers/uploads.js
@@ -3,20 +3,25 @@ const formidable = require("formidable");
 const path = require("path");
 const { rename } = require("fs");
 const { randomBytes } = require("crypto");
+const { BadRequestError, PayloadError } = require("../errors");
 
 const fileName = () => randomBytes(8).toString("hex");
 
 const uploadProductImage = (req, res) => {
   const form = new formidable.IncomingForm();
   const maxFileSize = 1 * 1024 * 1024; // 1mb
+  const minFileSize = 1 * 1024; // 1kb
   let filename;
 
   form.uploadDir = path.join(__dirname, "../public/uploads");
 
   form.on("progress", (bytesReceived, bytesExpected) => {
-    if (bytesExpected > maxFileSize) {
-      throw new Error(`File is too large, expectBytes: ${maxFileSize}`);
-    }
+    if (bytesExpected < minFileSize)
+      throw new BadRequestError(
+        `File is too small, expectBytes: ${minFileSize} or more`
+      );
+    if (bytesExpected > maxFileSize)
+      throw new PayloadError(`File is too large, expectBytes: ${maxFileSize}`);
   });
 
   form.on("file", (field, file) => {
@@ -31,7 +36,7 @@ const uploadProductImage = (req, res) => {
   });
 
   form.on("end", () =>
-    res.status(status("OK")).json({ image: { src: `/uploads/${filename}` } })
+    res.status(status("OK")).json({ image: { src: filename } })
   );
 
   form.parse(req);
